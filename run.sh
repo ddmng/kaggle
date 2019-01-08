f22d304afcfd2e2f2ede909c949900da78605db0
ddmng <daniele.diminica@gmail.com>
2019-01-08 22:19:31 +0100

    copied some exmaple

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+
+# Used to run without container
+function run_local {
+	jupyter notebook
+}
+
+# Spins up a kaggle container
 docker run -v $PWD:/tmp/working -w=/tmp/working -p 8889:8888 --name kaggle -it gcr.io/kaggle-images/python jupyter notebook --no-browser --ip="0.0.0.0" --notebook-dir=/tmp/working --allow-root
+
+# if returns an error, run local command
+if [[ $? == *cpu_feature_guard* ]] ; then docker rm kaggle && run_local(); fi
+
