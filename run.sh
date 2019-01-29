24f3cab533ba30868868633fe5fba8097282a688
ddmng <daniele.diminica@gmail.com>
2019-01-29 14:55:42 +0100

    rare labels and common labels

--- run.sh
+++ run.sh
@@ -9,6 +9,4 @@ function run_local {
 # Spins up a kaggle container
 docker run -v $PWD:/tmp/working -w=/tmp/working -p 8889:8888 --name kaggle -it gcr.io/kaggle-images/python jupyter notebook --no-browser --ip="0.0.0.0" --notebook-dir=/tmp/working --allow-root
 
-# if returns an error, run local command
-if [[ $? == *cpu_feature_guard* ]] ; then docker rm kaggle && run_local(); fi
 
